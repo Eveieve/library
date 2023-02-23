cc5f40ebdc320df63152ddb80ea078185653dd0b
Mike <nhj25252@gmail.com>
2023-02-23 20:18:31 +0900

    [feat] Set a constructor for creating book objects

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.info = function () {
+    return `${this.name} is by ${this.author}, ${this.pages}pages.`;
+  };
+}
