d60be4c466cb8251bd9798372223619daa1660cb
Mike <nhj25252@gmail.com>
2023-02-28 11:08:59 +0900

    [feat] Take user's book input to an array

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function Book(title, author, pages, isRead) {
     return `${this.name} is by ${this.author}, ${this.pages}pages.`;
   };
 }
+
+let userInput;
+
+const add = document.querySelector(".add");
+
+function addBookToLibrary() {
+  userInput = prompt("Which?");
+  //push userInput into the array, making it an array of book names
+  myLibrary.push(userInput);
+  console.log(myLibrary);
+  return myLibrary;
+}
+
+add.addEventListener("click", addBookToLibrary);
