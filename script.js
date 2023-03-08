d63caccf01a85b2a4baf855403091d3f50af1466
Mike <nhj25252@gmail.com>
2023-03-08 10:58:07 +0900

    wip

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  // this.remove = "Remove";
 }
-
-const add = document.querySelector(".add");
 
 function removeBook(index) {
   myLibrary = myLibrary.splice(index, 1);
-  displayBooks(); //different from displayBook?
+  displayBooks();
 }
 
 function toggle(index) {
@@ -26,6 +23,7 @@ function addBookToLibrary() {
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let isRead = document.getElementById("read");
+
   if (isRead.checked === true) {
     isRead = "Read";
   } else isRead = "Not read yet";
@@ -33,13 +31,12 @@ function addBookToLibrary() {
   const book = new Book(title, author, pages, isRead);
 
   myLibrary.push(book);
-  console.log(myLibrary.count);
+
   return myLibrary;
 }
 const shelf = document.querySelector(".shelf");
 const addBtn = document.querySelector(".add");
 const modalForm = document.querySelector(".modal");
-
 const form = document.querySelector("form");
 // console.log(form);
 
@@ -56,6 +53,7 @@ function displayBook(book, index) {
 
   eachBook.classList.add("eachBook");
   // document.body.appendChild(eachBook);
+  shelf.appendChild(eachBook);
 
   for (const property in book) {
     const bookProperty = document.createElement("p");
@@ -63,24 +61,36 @@ function displayBook(book, index) {
     bookProperty.textContent = `${book[property]}`;
     eachBook.appendChild(bookProperty);
   }
-  //Create remove button when display each Book too!
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
-  eachBook.appendChild(removeBtn);
-  // const removeBtn = document.querySelector(".eachBook p:nth-child(5)");
 
+  //Create remove button when display each Book too!
+  //All that are happening within eachBook card , goes here
+  const removeBtn = document.createElement("button");
+
+  removeBtn.style.cssText =
+    "background-color: rgb(79, 160, 149); color: white; border: none; font-size: medium; font-weight: 700; border-radius: .3rem; width: 8rem; height: 2.5rem";
+  removeBtn.textContent = "Remove";
+  // removeBtn.classList.add = ("removeBtn", "property");
+  eachBook.appendChild(removeBtn);
   removeBtn.addEventListener("click", () => {
     removeBook(index);
   });
+  //Toggle
+  const readToggle = eachBook.querySelector("p:nth-child(4)");
+
+  console.log(readToggle); //null why???
+  card.style.cssText =
+    "background-color: rgb(186, 209, 194); text-align: center; min-width:10rem; border-radius:.5rem; padding: 2rem;";
+
+  readToggle.addEventListener("click", () => toggle(index));
+
+  document.querySelector("form").reset();
 }
 
-const readToggle = document.querySelector(".eachBook p:nth-child(4)");
 addBtn.addEventListener("click", () => {
   modalForm.showModal();
 });
 
 form.addEventListener("submit", () => {
   addBookToLibrary();
-  form.reset();
-  displayBooks();
+  displayBook();
 });
