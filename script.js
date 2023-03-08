62ca3df145b9c2f54bd9283444148eea68a0ec5d
Mike <nhj25252@gmail.com>
2023-03-09 04:01:59 +0900

    wip

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let myLibrary = [];
+/* eslint-disable operator-linebreak */
+const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -7,21 +8,10 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-function removeBook(index) {
-  myLibrary = myLibrary.splice(index, 1);
-  displayBooks();
-}
-
-function toggle(index) {
-  myLibrary[index].isRead =
-    myLibrary[index].isRead === "Read" ? "Not read yet" : "Read";
-  displayBooks();
-}
-
 function addBookToLibrary() {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
   let isRead = document.getElementById("read");
 
   if (isRead.checked === true) {
@@ -34,63 +24,66 @@ function addBookToLibrary() {
 
   return myLibrary;
 }
-const shelf = document.querySelector(".shelf");
-const addBtn = document.querySelector(".add");
-const modalForm = document.querySelector(".modal");
-const form = document.querySelector("form");
-// console.log(form);
+const container = document.querySelector(".container");
+const add = document.querySelector(".add");
 
-const displayBooks = () => {
-  while (shelf.childElementCount > 1) shelf.lastChild.remove();
-  //Call displayBook for element in the array
-  myLibrary.forEach(displayBook);
-};
-
-function displayBook(book, index) {
-  const eachBook = document.createElement("div");
-  //give eachBook an index
-  eachBook.dataset.index = index;
-
-  eachBook.classList.add("eachBook");
-  // document.body.appendChild(eachBook);
-  shelf.appendChild(eachBook);
-
+function displayBook(book) {
+  const card = document.createElement("div");
   for (const property in book) {
-    const bookProperty = document.createElement("p");
-    //loop through properties of each Book item
-    bookProperty.textContent = `${book[property]}`;
-    eachBook.appendChild(bookProperty);
+    const bookEl = document.createElement("p");
+
+    bookEl.textContent = `${book[property]}`;
+
+    card.appendChild(bookEl);
   }
 
-  //Create remove button when display each Book too!
-  //All that are happening within eachBook card , goes here
-  const removeBtn = document.createElement("button");
+  // create removeBtn
+  const remove = document.createElement("button");
 
-  removeBtn.style.cssText =
+  remove.style.cssText =
     "background-color: rgb(79, 160, 149); color: white; border: none; font-size: medium; font-weight: 700; border-radius: .3rem; width: 8rem; height: 2.5rem";
-  removeBtn.textContent = "Remove";
-  // removeBtn.classList.add = ("removeBtn", "property");
-  eachBook.appendChild(removeBtn);
-  removeBtn.addEventListener("click", () => {
-    removeBook(index);
-  });
-  //Toggle
-  const readToggle = eachBook.querySelector("p:nth-child(4)");
+  remove.textContent = "Remove";
+  remove.classList.add = ("removeBtn", "property");
+  card.appendChild(remove);
 
-  console.log(readToggle); //null why???
+  function removeBook() {
+    // remove one Book from the array
+    // eslint-disable-next-line no-use-before-define
+    displayBooks(); //
+  }
+
+  function toggle(index) {
+    // Book at certain index...
+    myLibrary[index].isRead =
+      myLibrary[index].isRead === "Read" ? "Not read yet" : "Read";
+    // eslint-disable-next-line no-use-before-define
+    displayBooks();
+  }
+
+  remove.addEventListener("click", () => removeBook());
+  // Toggle
+  const readToggle = card.querySelector("p:nth-child(4)");
+  readToggle.addEventListener("click", () => toggle());
+
+  container.appendChild(card);
   card.style.cssText =
     "background-color: rgb(186, 209, 194); text-align: center; min-width:10rem; border-radius:.5rem; padding: 2rem;";
 
-  readToggle.addEventListener("click", () => toggle(index));
-
   document.querySelector("form").reset();
 }
+const displayBooks = () => {
+  while (container.childElementCount > 1) container.lastChild.remove();
+  myLibrary.forEach(displayBook);
+};
 
-addBtn.addEventListener("click", () => {
-  modalForm.showModal();
+const modal = document.querySelector(".modal");
+
+add.addEventListener("click", () => {
+  modal.showModal();
 });
 
+const form = document.querySelector("form");
 form.addEventListener("submit", () => {
   addBookToLibrary();
-  displayBook();
+  displayBooks();
 });
